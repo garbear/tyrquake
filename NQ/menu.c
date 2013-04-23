garbear/tyrquake

commit 0a482f2551b365f9d7341e50db577952bc53275a

(MSVC) More MSVC C++ fixes

NQ/menu.c
@@ -1509,7 +1509,7 @@ M_Quit_Key(int key)
     case 'n':
     case 'N':
 	if (wasInMenus) {
-	    m_state = m_quit_prevstate;
+	    m_state = (m_state_enum)m_quit_prevstate;
 	    m_entersound = true;
 	} else {
 	    key_dest = key_game;
@@ -1534,7 +1534,7 @@ static void
 M_Quit_Draw(void)
 {
     if (wasInMenus) {
-	m_state = m_quit_prevstate;
+	m_state = (m_state_enum)m_quit_prevstate;
 	m_recursiveDraw = true;
 	M_Draw();
 	m_state = m_quit;

NQ/cl_demo.c
@@ -318,8 +318,13 @@ CL_Demo_Arg_f(const char *arg)
     struct stree_root *root;
 
     root = (struct stree_root*)Z_Malloc(sizeof(struct stree_root));
-    if (root) {
-	*root = STREE_ROOT;
+    if (root)
+    {
+    root->entries = 0;
+    root->maxlen = 0;
+    root->minlen = -1;
+    //root->root = NULL;
+    root->stack = NULL;
 	STree_AllocInit();
 	COM_ScanDir(root, "", arg, ".dem", true);
     }

NQ/host_cmd.c
@@ -305,7 +305,11 @@ Host_Map_Arg_f(const char *arg)
 
     root = (struct stree_root*)Z_Malloc(sizeof(struct stree_root));
     if (root) {
-	*root = STREE_ROOT;
+    root->entries = 0;
+    root->maxlen = 0;
+    root->minlen = -1;
+    //root->root = NULL;
+    root->stack = NULL;
 
 	STree_AllocInit();
 	COM_ScanDir(root, "maps", arg, ".bsp", true);

NQ/menu.h
@@ -44,7 +44,7 @@ void M_PrintWhite(int cx, int cy, const char *str);
 extern qboolean m_return_onerror;
 extern char m_return_reason[32];
 extern int m_return_state;
-enum {
+enum m_state_enum {
     m_none, m_main, m_singleplayer, m_load, m_save, m_multiplayer, m_setup,
     m_options, m_video, m_keys, m_help, m_quit, m_lanconfig, m_gameoptions,
     m_search, m_slist

NQ/sv_main.c
@@ -48,13 +48,13 @@ typedef struct {
     const char *description;
 } sv_protocol_t;
 
-#define PROT(v, n, d) { .version = v, .name = n, .description = d }
+#define PROT(v, n, d) { v, n, d }
 static sv_protocol_t sv_protocols[] = {
-    PROT(PROTOCOL_VERSION_NQ,   "nq",   "Standard NetQuake protocol"),
-    PROT(PROTOCOL_VERSION_FITZ, "fitz", "FitzQuake protocol"),
-    PROT(PROTOCOL_VERSION_BJP,  "bjp",  "BJP protocol (v1)"),
-    PROT(PROTOCOL_VERSION_BJP2, "bjp2", "BJP protocol (v2)"),
-    PROT(PROTOCOL_VERSION_BJP3, "bjp3", "BJP protocol (v3)"),
+    {PROTOCOL_VERSION_NQ,   "nq",   "Standard NetQuake protocol"},
+    {PROTOCOL_VERSION_FITZ, "fitz", "FitzQuake protocol"},
+    {PROTOCOL_VERSION_BJP,  "bjp",  "BJP protocol (v1)"},
+    {PROTOCOL_VERSION_BJP2, "bjp2", "BJP protocol (v2)"},
+    {PROTOCOL_VERSION_BJP3, "bjp3", "BJP protocol (v3)"},
 };
 
 static int sv_protocol = PROTOCOL_VERSION_NQ;
@@ -116,8 +116,13 @@ SV_Protocol_Arg_f(const char *arg)
 
     root = (struct stree_root*)Z_Malloc(sizeof(struct stree_root));
     if (root) {
-	*root = STREE_ROOT;
-	STree_AllocInit();
+	root->entries = 0;
+    root->maxlen = 0;
+    root->minlen = -1;
+    //root->root = {NULL};
+    root->stack = NULL;
+
+    STree_AllocInit();
 	arg_len = arg ? strlen(arg) : 0;
 	for (i = 0; i < ARRAY_SIZE(sv_protocols); i++) {
 	    if (!arg || !strncasecmp(sv_protocols[i].name, arg, arg_len))

common/cmd.c
@@ -858,7 +858,12 @@ struct stree_root *Cmd_CommandCompletions(const char *buf)
     struct stree_root *root_tree;
 
     root_tree = (struct stree_root*)Z_Malloc(sizeof(struct stree_root));
-    *root_tree = STREE_ROOT;
+
+    root_tree->entries = 0;
+    root_tree->maxlen = 0;
+    root_tree->minlen = -1;
+    root_tree->root = {NULL};
+    root_tree->stack = NULL;
 
     STree_AllocInit();
 

common/common.c
@@ -1296,15 +1296,17 @@ COM_AddParm(const char *parm)
 COM_Init
 ================
 */
+
+    union swaptest {
+	byte b[2];
+	short s;
+    } swaptest;
+
 void
 COM_Init(void)
 {
-    union {
-	byte b[2];
-	short s;
-    } swaptest = {
-	.b = { 1, 0 }
-    };
+   swaptest.b[0] = 1;
+   swaptest.b[1] = 0;
 
 // set the byte swapping variables in a portable manner
     if (swaptest.s == 1) {
@@ -1598,8 +1600,7 @@ COM_FOpenFile(const char *filename, FILE **file)
     return -1;
 }
 
-static void
-COM_ScanDirDir(struct stree_root *root, DIR *dir, const char *pfx,
+static void COM_ScanDirDir(struct stree_root *root, DIR *dir, const char *pfx,
 	       const char *ext, qboolean stripext)
 {
     int pfx_len, ext_len;

common/console.c
@@ -750,8 +750,14 @@ Con_ShowTree(struct stree_root *root)
 void
 Con_Maplist_f()
 {
-    struct stree_root st_root = STREE_ROOT;
+    struct stree_root st_root;
     const char *pfx = NULL;
+
+    st_root.entries = 0;
+    st_root.maxlen = 0;
+    st_root.minlen = -1;
+    //st_root.root = NULL;
+    st_root.stack = NULL;
 
     if (Cmd_Argc() == 2)
 	pfx = Cmd_Argv(1);
@@ -772,7 +778,7 @@ Con_Init(void)
 {
     debuglog = COM_CheckParm("-condebug");
 
-    con_main.text = Hunk_AllocName(CON_TEXTSIZE, "conmain");
+    con_main.text = (char*)Hunk_AllocName(CON_TEXTSIZE, "conmain");
 
     con = &con_main;
     con_linewidth = -1;

commit a6bd55ddf6f373e2b0433bec8783b231002346d6

Fixed mingw compile error introduced in 0a482f255: 'm_state_enum' undeclared identifier, explicit cast isn't necessary so only include it when MSVC C++ fix is needed

NQ/menu.c
@@ -1555,7 +1555,11 @@ M_Quit_Key(int key)
     case 'n':
     case 'N':
 	if (wasInMenus) {
+#ifdef _MSC_VER
 	    m_state = (m_state_enum)m_quit_prevstate;
+#else
+	    m_state = m_quit_prevstate;
+#endif
 	    m_entersound = true;
 	} else {
 	    key_dest = key_game;
@@ -1584,7 +1588,11 @@ static void
 M_Quit_Draw(void)
 {
     if (wasInMenus) {
+#ifdef _MSC_VER
 	m_state = (m_state_enum)m_quit_prevstate;
+#else
+	m_state = m_quit_prevstate;
+#endif
 	m_recursiveDraw = true;
 	M_Draw();
 	m_state = m_quit;